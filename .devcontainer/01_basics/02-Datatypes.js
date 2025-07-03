cb0c6156759a6e16655196b41c47fd6a9f43370c
Ankita Marik <147146659+Ankita0203@users.noreply.github.com>
2025-07-03 17:08:45 +0000

    Datatypes

---
+++ .devcontainer/01_basics/02-Datatypes.js
@@ -0,0 +1,11 @@
+"use Strict";//treats all js code as newer version
+//alert(3+3) we r using node.js not browsewr
+
+console.log(3+3)
+console.log("Ankita")
+//null: standalone value (an object)
+//undefined:vakues yet to assigned (a type)
+//symbol:unique
+//bigint:represents very large no..
+console.log(typeof undefined)
+console.log(typeof null)
