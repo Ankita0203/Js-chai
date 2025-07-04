cfa30026d8c34905b5b610f56d45a19439536b5d
Ankita Marik <147146659+Ankita0203@users.noreply.github.com>
2025-07-04 16:21:12 +0000

    git add 03_ConversionOperation git commit -m "Update 03_ConversionOperation with fixes"

---
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -0,0 +1,19 @@
+let score = "Ankita";
+let valueInNumber = Number(score);
+console.log(typeof score);               // "string"
+console.log(typeof valueInNumber);       // "number"
+console.log(valueInNumber);              // NaN
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+//let isLogged = 1;
+let isLogged = "";
+let booleanIsLoggedIn = Boolean(isLogged);
+//console.log(booleanIsLoggedIn);          // true
+
+let someNumber=33
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber); //to know the datatype
