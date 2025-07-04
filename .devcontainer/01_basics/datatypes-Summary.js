f4cbbaad0ef16f1e3ba98ceab928e761425c40d6
Ankita Marik <147146659+Ankita0203@users.noreply.github.com>
2025-07-04 18:00:50 +0000

    datatypes-Summary.js

---
+++ .devcontainer/01_basics/datatypes-Summary.js
@@ -0,0 +1,19 @@
+//7 types:
+//String,Nubner,Boolean,Null,Undefined,symbol,BigInt
+//Undefined:Variable declared ,memory space is declared but value is not known !!
+//Non premivite:Arrays,objects,Functions
+
+//dynamiocally types (no need to predeclare types)
+//Array:
+const heroes=["Shaktiman","Dogman","Spiderman"];
+//object:
+let myObj=
+{name:"Ankita",
+    age:22,
+}
+//const is used for variables:
+//function:
+const myFunction=function(){
+    console.log("Hello world");
+}
+console.log(typeof bignumber);//type of is used to know datatype
